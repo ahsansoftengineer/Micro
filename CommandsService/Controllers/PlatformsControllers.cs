c0109cbd6d3071c82e3f36fd6a379981fd92a2be
Muhammad Ahsan Saifi <ahsansoftengineer@gmail.com>
2024-04-12 20:14:51 +0500

    MESSAGIN DEMONSTRATED

---
+++ CommandsService/Controllers/PlatformsControllers.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers 
+{
+
+  [Route("api/c/[controller]")]
+  [ApiController]
+  public class PlatformsController : ControllerBase 
+  {
+    public PlatformsController()
+    {
+      
+    }
+    [HttpPost]
+    public ActionResult TestInboundConnection() 
+    {
+      Console.WriteLine("--> Inbound POST # Command Service");
+
+      return Ok("Inbound test of from Platforms Controller");
+    }
+  }
+}
