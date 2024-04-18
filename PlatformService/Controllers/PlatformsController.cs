3bb4f7c51adf53469f199d8543af7df2be773869
Muhammad Ahsan Saifi <ahsansoftengineer@gmail.com>
2024-04-18 18:08:09 +0500

    MESSAGE SEND FROM PLATFORM TO RabbitMQ

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -5,6 +5,7 @@ using PlatformService.DATA;
 using PlatformService.DTO;
 using PlatformService.MODELS;
 using PlatformService.DataServiceSyncs.Http;
+using PlatformService.DataServiceAsync;
 
 namespace PlatformService.Controllers
 {
@@ -16,17 +17,20 @@ namespace PlatformService.Controllers
     private readonly IMapper _mapper;
     private readonly ILogger<PlatformController> _logger;
     private readonly ICommandDataClient _cmdDataClient;
+    private readonly IMessageBusClient _messageBusClient;
 
     public PlatformController(
-  IPlatformRepo repo,
-  IMapper mapper,
-  ICommandDataClient cmdDataClient,
-  ILogger<PlatformController> logger)
+      IPlatformRepo repo,
+      IMapper mapper,
+      ICommandDataClient cmdDataClient,
+      ILogger<PlatformController> logger,
+      IMessageBusClient messageBusClient)
     {
       _repo = repo;
       _mapper = mapper;
       _logger = logger;
       _cmdDataClient = cmdDataClient;
+      _messageBusClient = messageBusClient;
     }
 
     [HttpGet]
@@ -59,13 +63,27 @@ namespace PlatformService.Controllers
       var read = _mapper.Map<PlatformReadDto>(model);
       // return Ok(read);
 
-      try {
+      // Send Sync Message
+      try
+      {
         await _cmdDataClient.SendPlatformToCommand(read);
-      } catch(Exception e) {
-        Console.WriteLine("--> Could not send synchronously "  + e.Message);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("--> Could not send synchronously " + ex.Message);
       }
 
-
+      // Send Async Message
+      try
+      {
+        var pubDto = _mapper.Map<PlatformPublishedDto>(read);
+        pubDto.Event = "Platform_Published";
+        _messageBusClient.PublishNewPlatform(pubDto);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("--> Could not send Async Msg: " + ex.Message);
+      }
 
       return CreatedAtRoute(
         nameof(GetPlatformById),
